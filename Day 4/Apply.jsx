4d84e9b16108fea50eb2c28314a6dc7ef35c0986
Praveen009829 <119106306+Praveen009829@users.noreply.github.com>
2024-03-20 14:54:12 +0530

    Add files via upload

--- Day 4/Apply.jsx
+++ Day 4/Apply.jsx
@@ -4,87 +4,31 @@ import {
   MDBContainer,
   MDBCard,
   MDBCardBody,
-  MDBCardImage,
-  MDBRow,
-  MDBCol,
   MDBInput,
-  MDBRadio
+  MDBCheckbox
 }
 from 'mdb-react-ui-kit';
-import {Link} from 'react-router-dom';
+import './Apply.css'
+import {Link } from 'react-router-dom';
 
 function App() {
   return (
-    <MDBContainer fluid className='bg-dark'>
-
-      <MDBRow className='d-flex justify-content-center align-items-center h-100'>
-        <MDBCol>
-
-          <MDBCard className='my-4'>
-
-            <MDBRow className='g-0'>
-
-              <MDBCol md='4' className="d-none d-md-block">
-                <MDBCardImage src='https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/img4.webp' alt="Sample photo" className="rounded-start" fluid/>
-              </MDBCol>
-
-              <MDBCol md='6'>
-
-                <MDBCardBody className='text-black d-flex flex-column justify-content-center'>
-                  <h3 className="mb-5 text-uppercase fw-bold">Application form</h3>
-
-                  <MDBRow>
-
-                    <MDBCol md='6'>
-                      <MDBInput wrapperClass='mb-4' label='First Name' size='lg' id='form1' type='text'/>
-                    </MDBCol>
-
-                    <MDBCol md='6'>
-                      <MDBInput wrapperClass='mb-4' label='Last Name' size='lg' id='form2' type='text'/>
-                    </MDBCol>
-
-                  </MDBRow>
-
-                  <MDBInput wrapperClass='mb-4' label='Birthday' size='lg' id='form3' type='text'/>
-
-                  <div className='d-md-flex ustify-content-start align-items-center mb-4'>
-                    <h6 class="fw-bold mb-0 me-4">Gender: </h6>
-                    <MDBRadio name='inlineRadio' id='inlineRadio1' value='option1' label='Female' inline />
-                    <MDBRadio name='inlineRadio' id='inlineRadio2' value='option2' label='Male' inline />
-                    <MDBRadio name='inlineRadio' id='inlineRadio3' value='option3' label='Other' inline />
-                  </div>
-
-                  <MDBRow>
-
-                    <MDBCol md='6'>
-                    <MDBInput wrapperClass='mb-4' label='State' size='lg' id='form3' type='text'/>
-                    </MDBCol>
-
-                    <MDBCol md='6'>
-                    <MDBInput wrapperClass='mb-4' label='City' size='lg' id='form3' type='text'/>
-                    </MDBCol>
-
-                  </MDBRow>
-
-                  <MDBInput wrapperClass='mb-4' label='Pincode' size='lg' id='form4' type='text'/>
-                  <MDBInput wrapperClass='mb-4' label='Course' size='lg' id='form5' type='text'/>
-                  <MDBInput wrapperClass='mb-4' label='Email ID' size='lg' id='form6' type='text'/>
-
-                  <div className="d-flex justify-content-end pt-3">
-                    <MDBBtn color='light' size='lg'>Reset all</MDBBtn>
-                    <MDBBtn className='ms-2' color='warning' size='lg'><Link to='/user'>Submit</Link></MDBBtn>
-                  </div>
-
-                </MDBCardBody>
-
-              </MDBCol>
-            </MDBRow>
-
-          </MDBCard>
-
-        </MDBCol>
-      </MDBRow>
-
+    <MDBContainer fluid className='d-flex align-items-center justify-content-center bg-image' style={{backgroundImage: 'url(https://mdbcdn.b-cdn.net/img/Photos/new-templates/search-box/img4.webp)'}}>
+      <div className='mask gradient-custom-3'></div>
+      <MDBCard className='m-5' style={{maxWidth: '600px'}}>
+        <MDBCardBody className='px-5'>
+          <h2 className="text-uppercase text-center mb-5">Apply for Job Posting</h2>
+          <MDBInput wrapperClass='mb-4' label='Application Name' size='lg' id='form1' type='text' required/>
+          <MDBInput wrapperClass='mb-4' label='Your Email' size='lg' id='form2' type='email' required/>
+          <MDBInput wrapperClass='mb-4' label='Contact Number' size='lg' id='form2' type='number' required/>
+          <MDBInput wrapperClass='mb-4' label='Amount' size='lg' id='form2' type='number' required/>
+          <MDBInput wrapperClass='mb-4' label='Job Title' size='lg' id='form2' type='text' required/>
+          <div className='d-flex flex-row justify-content-center mb-4'>
+            <MDBCheckbox name='flexCheck' id='flexCheckDefault' label='I agree all statements in Terms of service' required/>
+          </div>
+          <Link to='/applications'><MDBBtn className='mb-4 w-100 gradient-custom-4' size='lg'>Submit</MDBBtn></Link>
+        </MDBCardBody>
+      </MDBCard>
     </MDBContainer>
   );
 }
